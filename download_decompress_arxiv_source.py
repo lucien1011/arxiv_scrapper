4a06335feb66b9fee128c4dcaa316efa7ce9860d
Lucien <lucien@luciens-macbook-pro-1.home>
2021-11-07 23:05:32 +0100

    Added reverse and save_bib_only flag

--- download_decompress_arxiv_source.py
+++ download_decompress_arxiv_source.py
@@ -1,9 +1,11 @@
 import arxiv
+import glob
 import gzip
 import json
 import os
 import pandas as pd
 from tqdm import tqdm
+import shutil
 
 from utils import mkdir_p
 
@@ -15,6 +17,7 @@ def parse_arguments():
     parser.add_argument('--json',action='store',type=str,default='')
     parser.add_argument('--out_dir',action='store',type=str)
     parser.add_argument('--df_path',action='store',type=str)
+    parser.add_argument('--save_bib_only',action='store_true')
     return parser.parse_args()
 
 # ______________________________________________________________________________________________________ ||
@@ -53,20 +56,27 @@ def read_meta_json(json_path,out_dir):
         mkdir_p(id_out_dir)
         download_uncompress_source_files(id_str,id_out_dir)
 
-def read_df(df_path,out_dir,):
+def read_df(df_path,out_dir,save_bib_only=False,reverse=True):
     df = pd.read_csv(df_path,index_col=None)
+    if reverse:
+        df = df.iloc[::-1]
     for index,row in df.iterrows():
         id_str = row['id']
         id_out_dir = os.path.join(out_dir,id_str)
+        if "/" in id_str: continue
+        if os.path.exists(id_out_dir): continue
         mkdir_p(id_out_dir)
         download_uncompress_source_files(id_str,id_out_dir)
+        bib_file_paths = glob.glob(os.path.join(id_out_dir,'*.bib')) + glob.glob(os.path.join(id_out_dir,'*.no-bib'))
+        if save_bib_only and not bib_file_paths:
+            print("No bib file, removing ",id_out_dir)
+            shutil.rmtree(id_out_dir)
     
 if __name__ == "__main__":
     args = parse_arguments()
     if args.df_path:
-        read_df(args.df_path,args.out_dir)    
+        read_df(args.df_path,args.out_dir,args.save_bib_only)
     elif args.id:
         download_uncompress_source_files(args.id,args.out_dir)
     elif args.json:
         read_meta_json(args.json,args.out_dir)
-        
